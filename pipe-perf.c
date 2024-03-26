0c24c61ddb8da4b2c8fd1f2283c54f1003d9818f
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-03-26 13:45:09 -0400

    add pipe-perf.c

---
+++ pipe-perf.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include "lind_platform.h"
+
+unsigned long long WRITE_BUFFER_SIZE = 1UL << 16;
+unsigned long long NUMBER_OF_WRITES = 1UL << (30 - 14);
+unsigned long long READ_BUFFER_SIZE = 1UL << 16;
+unsigned long long OUTLOOP = 1UL << 4;
+
+pthread_barrier_t barrier; // wait for buffer allocation
+int fd[2];
+
+void* writerThreadFuncion(void *arg) {
+    (void)arg;
+
+    // NOTE: this is not a fork, this just create a new cage
+    // lind_fork
+
+    // preapre the write buffer
+    char *buffer = malloc(sizeof(char) * WRITE_BUFFER_SIZE);
+    for (int i = 0; i < WRITE_BUFFER_SIZE; i++) buffer[i] = 'A';
+
+    pthread_barrier_wait(&barrier);
+
+    for (int n = 0; n < OUTLOOP; n++)
+    {
+        for (int count = 0; count < NUMBER_OF_WRITES; count++)
+        {
+            lind_write(fd[1], buffer, WRITE_BUFFER_SIZE, 1);
+        }
+    }
+    free(buffer);
+    return NULL;
+}
+
+void* readerThreadFunction(void *arg) {
+    (void)arg;
+
+    // prepare the read buffer
+    char *buffer = (char *)calloc(READ_BUFFER_SIZE, sizeof(char));
+    
+    pthread_barrier_wait(&barrier);
+
+    while ((readlen = lind_read(fd[0], buffer, READ_BUFFER_SIZE, 1)) > 0) {}
+    
+    free(buffer);
+    return NULL;
+}
+
+int main(void)
+{
+    lindrustinit(0);
+    int ret;
+
+    // make the pipes
+    if (lind_pipe(fd, 1) < 0) {
+		perror("pipe()");
+		exit(EXIT_FAILURE);
+	}
+    
+    pthread_barrier_init(&barrier, NULL, 3);
+
+    // create the reader and writer threads
+    pthread_t writerThread, readerThread;
+
+    if(pthread_create(&writerThread, NULL, writerThreadFuncion, NULL)) {
+        perror("Failred to create writer thread!");
+        return 1;
+    }
+
+    if(pthread_create(&readerThread, NULL, readerThreadFunction, NULL)) {
+        perror("Failred to create reader thread!");
+        return 1;
+    }
+
+    // prepare the timer
+    clock_t start, end;
+    double cpu_time_used;
+
+    // wait for: writer is ready to write, reader is ready to reead
+    pthread_barrier_wait(&barrier);
+
+    start = clock();
+    pthread_join(&readerThread, NULL);
+    end = clock();
+    pthread_join(&writerThread, NULL);
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Pipe took %f seconds to execute \n", cpu_time_used);
+    return 0;
+}
